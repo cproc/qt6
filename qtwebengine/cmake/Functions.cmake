8fb56f58eaca4dd0b6152e55d9d0450add5668e6
Christian Prochaska <christian.prochaska@genode-labs.com>
2024-08-20 09:22:55 +0200

    qtwebengine_linux_crossbuild.patch

--- qtwebengine/cmake/Functions.cmake
+++ qtwebengine/cmake/Functions.cmake
@@ -1044,6 +1044,7 @@ macro(append_toolchain_setup)
         endif()
     elseif(LINUX OR FREEBSD)
         get_gn_arch(cpu ${TEST_architecture_arch})
+        get_gn_os(os)
         list(APPEND gnArgArg
             custom_toolchain="${buildDir}/target_toolchain:target"
             host_toolchain="${buildDir}/host_toolchain:host"
@@ -1052,6 +1053,7 @@ macro(append_toolchain_setup)
             list(APPEND gnArgArg
                 v8_snapshot_toolchain="${buildDir}/v8_toolchain:v8"
                 target_cpu="${cpu}"
+                target_os="${os}"
             )
         else()
             list(APPEND gnArgArg host_cpu="${cpu}")
