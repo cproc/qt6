e9da0be004c401907b1cc1ab4eb1223b241bd130
Christian Prochaska <christian.prochaska@genode-labs.com>
2024-09-27 16:25:49 +0200

    qtbase_genode_qpa.patch

---
+++ qtbase/src/plugins/platforms/genode/qgenodescreen.h
@@ -0,0 +1,108 @@
+/*
+ * \brief  QGenodeScreen
+ * \author Christian Prochaska
+ * \date   2013-05-08
+ */
+
+/*
+ * Copyright (C) 2013-2023 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+
+#ifndef _QGENODESCREEN_H_
+#define _QGENODESCREEN_H_
+
+/* Genode includes */
+#include <gui_session/connection.h>
+
+/* Qt includes */
+#include <qpa/qplatformscreen.h>
+#include <qpa/qwindowsysteminterface.h>
+
+#include <QDebug>
+
+#include "qgenodecursor.h"
+#include "qgenodesignalproxythread.h"
+
+QT_BEGIN_NAMESPACE
+
+class QGenodeScreen : public QObject, public QPlatformScreen
+{
+	Q_OBJECT
+
+	private:
+
+		Genode::Env              &_env;
+		QRect                     _geometry;
+
+		QGenodeSignalProxyThread &_signal_proxy;
+
+		Gui::Connection           _gui { _env };
+
+		Genode::Io_signal_handler<QGenodeScreen>
+			_mode_changed_signal_handler{_env.ep(), *this,
+			                             &QGenodeScreen::_handle_mode_changed};
+
+		void _handle_mode_changed()
+		{
+			_signal_proxy.screen_mode_changed();
+		}
+
+	private slots:
+
+		void _mode_changed()
+		{
+			Framebuffer::Mode const screen_mode = _gui.mode();
+
+			_geometry.setRect(0, 0, screen_mode.area.w,
+			                        screen_mode.area.h);
+
+			QWindowSystemInterface::handleScreenGeometryChange(screen(),
+			                                                   _geometry,
+			                                                   _geometry);
+		}
+
+	public:
+
+		QGenodeScreen(Genode::Env &env,
+		              QGenodeSignalProxyThread &signal_proxy)
+		: _env(env),
+		  _signal_proxy(signal_proxy)
+		{
+			_gui.mode_sigh(_mode_changed_signal_handler);
+
+			connect(&_signal_proxy, SIGNAL(screen_mode_changed_signal()),
+			        this, SLOT(_mode_changed()),
+			        Qt::QueuedConnection);
+
+			Framebuffer::Mode const scr_mode = _gui.mode();
+
+			_geometry.setRect(0, 0, scr_mode.area.w,
+			                        scr_mode.area.h);
+		}
+
+		QRect geometry() const override { return _geometry; }
+		int depth() const override { return 32; }
+		QImage::Format format() const override{ return QImage::Format_RGB32; }
+		QDpi logicalDpi() const override { return QDpi(80, 80); };
+
+		QSizeF physicalSize() const override
+		{
+			/* 'overrideDpi()' takes 'QT_FONT_DPI' into account */
+			static const int dpi = overrideDpi(logicalDpi()).first;
+			return QSizeF(geometry().size()) / dpi * qreal(25.4);
+		}
+
+		QPlatformCursor *cursor() const override
+		{
+			static QGenodeCursor instance(_env);
+			return &instance;
+		}
+};
+
+QT_END_NAMESPACE
+
+#endif /* _QGENODESCREEN_H_ */
